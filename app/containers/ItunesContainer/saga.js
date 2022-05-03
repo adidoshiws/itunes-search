5ab1e2fad9858c2e4e6e9eb5755fe087aeb95db7
adidoshiws <aditya@wednesday.is>
2022-05-03 16:20:10 +0530

    refactor: add try/catch block

--- app/containers/ItunesContainer/saga.js
+++ app/containers/ItunesContainer/saga.js
@@ -7,12 +7,14 @@ const { REQUEST_GET_ITUNE_TRACKS } = itunesContainerTypes;
 const { successGetItuneTracks, failureGetItuneTracks } = itunesContainerCreators;
 
 export function* getItuneTracks(action) {
-  const response = yield call(getTracks, action.trackName);
-  const { data, ok } = response;
-  if (ok) {
-    yield put(successGetItuneTracks(data));
-  } else {
-    yield put(failureGetItuneTracks(data));
+  try {
+    const response = yield call(getTracks, action.trackName);
+    const { data, ok } = response;
+    if (ok) {
+      yield put(successGetItuneTracks(data));
+    }
+  } catch (error) {
+    yield put(failureGetItuneTracks(error));
   }
 }
 
