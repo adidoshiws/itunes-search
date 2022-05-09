1dfd4de45df2c0961a8405a0171f679ccbd099d9
adidoshiws <aditya@wednesday.is>
2022-05-09 15:55:04 +0530

    refactor: update routeConfig file

--- app/routeConfig.js
+++ app/routeConfig.js
@@ -2,7 +2,6 @@ import NotFound from '@containers/NotFoundPage/Loadable';
 import HomeContainer from '@containers/HomeContainer/Loadable';
 import routeConstants from '@utils/routeConstants';
 import ItunesContainer from '@containers/ItunesContainer/Loadable';
-// import TrackDetails from '@components/TrackDetails/index';
 import TrackDetailsContainer from '@containers/TrackDetailsContainer/Loadable';
 export const routeConfig = {
   repos: {
