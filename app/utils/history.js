1dc1829e143ad926f0ba331559d1b7223693d6ba
adidoshiws <aditya@wednesday.is>
2022-05-10 12:19:27 +0530

    refactor: update history.js

--- app/utils/history.js
+++ app/utils/history.js
@@ -1,39 +1,17 @@
-// import { createBrowserHistory } from 'history';
-// import { isProd, isUAT } from './index';
-
-// export function getBaseName() {
-//   if (isProd()) {
-//     // GH Pages
-//     return '/react-template';
-//   }
-//   if (isUAT()) {
-//     return `/${process.env.BRANCH_NAME}`;
-//   }
-
-//   return '';
-// }
-
-// const history = createBrowserHistory({ basename: getBaseName() });
-// export default history;
-
 import { createBrowserHistory } from 'history';
-import routeConstants from '@utils/routeConstants';
-const routes = Object.keys(routeConstants);
-const pathname = window.location.pathname;
-let baseUrl = '';
-if (process.env.ENVIRONMENT_NAME === 'uat') {
-  routes.forEach((routeKey) => {
-    const route = routeConstants[routeKey].route;
-    if (pathname.includes(route)) {
-      if (pathname.substring(pathname.length - route.length, pathname.length) === route) {
-        baseUrl = pathname.substring(0, pathname.length - route.length);
-      }
-      if (pathname.substring(pathname.length - route.length, pathname.length - 1) === `${route}/`) {
-        baseUrl = pathname.substring(0, pathname.length - route.length - 1);
-      }
-    }
-  });
+import { isProd, isUAT } from './index';
+
+export function getBaseName() {
+  if (isProd()) {
+    // GH Pages
+    return '/react-template';
+  }
+  if (isUAT()) {
+    return `/${process.env.BRANCH_NAME}`;
+  }
+
+  return '';
 }
 
-const history = createBrowserHistory({ basename: baseUrl });
+const history = createBrowserHistory({ basename: getBaseName() });
 export default history;
